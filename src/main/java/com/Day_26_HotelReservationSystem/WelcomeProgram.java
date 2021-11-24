df6d223bfa6fde327c8420ddb173c5880a833b31
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-24 22:45:05 +0530

    Added: .gitignore and Welcome Files to Master

---
+++ src/main/java/com/Day_26_HotelReservationSystem/WelcomeProgram.java
@@ -0,0 +1,9 @@
+package com.Day_26_HotelReservationSystem;
+
+public class WelcomeProgram 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+}
