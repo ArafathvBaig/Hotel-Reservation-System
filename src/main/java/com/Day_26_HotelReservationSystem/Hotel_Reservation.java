87ca64d53b190f27ef11b4e388e1873395fac1ca
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-24 23:35:16 +0530

    Updated: Files With UC2

--- src/main/java/com/Day_26_HotelReservationSystem/Hotel_Reservation.java
+++ src/main/java/com/Day_26_HotelReservationSystem/Hotel_Reservation.java
@@ -1,7 +1,10 @@
 package com.Day_26_HotelReservationSystem;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 
 public class Hotel_Reservation 
@@ -22,6 +25,12 @@ public class Hotel_Reservation
 	{
 		return this.list;
 	}
+	
+	public  Hotels getCheapestHotel(LocalDate startDate, LocalDate endDate) 
+	{
+        Optional<Hotels> sortedHotelList = list.stream().min(Comparator.comparing(Hotels::getRegularRates));
+        return sortedHotelList.get();
+    }
 }
 
 
--- src/test/java/com/Day_26_HotelReservationSystem/Hotel_Reservation_Test.java
+++ src/test/java/com/Day_26_HotelReservationSystem/Hotel_Reservation_Test.java
@@ -1,5 +1,7 @@
 package com.Day_26_HotelReservationSystem;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
@@ -9,6 +11,7 @@ import junit.framework.Assert;
 public class Hotel_Reservation_Test 
 {
 	Hotel_Reservation hr = new Hotel_Reservation();
+	
 	@Test
 	public void addingDetailsOfHotelsAndTestingWeatherAddedOrNot() 
 	{
@@ -21,6 +24,21 @@ public class Hotel_Reservation_Test
 		List<Hotels> hotels = Arrays.asList(hotel01, hotel02, hotel03);
 		List<Hotels> result = hr.getHotels();
 		Assert.assertEquals(result, hotels);
+	}
+	
+	@Test
+	public void addingDetailsOfHotelsAndFindingCheapestHotelWithRegularRates() 
+	{
+		Hotels hotel01 = new Hotels("Lakewood", 220);
+		Hotels hotel02 = new Hotels("Bridgewood", 260);
+		Hotels hotel03 = new Hotels("Ridgewood", 330);
+		hr.addHotels(hotel01);
+		hr.addHotels(hotel02);
+		hr.addHotels(hotel03);
+		LocalDate startDate = LocalDate.of(2020, Month.SEPTEMBER, 10);
+	    LocalDate endDate = LocalDate.of(2020, Month.SEPTEMBER, 11);
+		Hotels hotel = hr.getCheapestHotel(startDate, endDate);
+		Assert.assertEquals("Lakewood", hotel.getHotelName());
 	}
 	
 }
